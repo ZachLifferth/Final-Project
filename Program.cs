ab8146e659de8cad0ffa05007ebdf182665f4303
Zach Lifferth <zachlifferth@gmail.com>
2022-04-09 22:19:38 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FinalProject 
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<string> searchResults = CoachSearch("Snow College");
+            foreach (var coach in searchResults)
+            {
+                Console.WriteLine(coach);
+            }
+        }
+        //This method will be able to go through the file
+        //After this is done it will return the coaches from the certain school
+        static List<string> CoachSearch(string school)
+        {
+            Debug.Assert(school.Length > 0, "No school Found");
+            return new List<string> {"Coach Long"};
+        }
+    }
+}
